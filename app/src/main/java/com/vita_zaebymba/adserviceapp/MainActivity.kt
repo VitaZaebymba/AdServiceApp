2f200bc169d72a9cec5b27f00d7ecb6146c3738f
Vita <malavita29@mail.ru>
2022-11-13 20:41:36 +0300

    add button_menu

--- app/src/main/java/com/vita_zaebymba/adserviceapp/MainActivity.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.vita_zaebymba.adserviceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
+
+    private fun init(){
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
 }
