c46c9c5edc7ff80e1ca6975dcba68d02c4e4329c
Vita <malavita29@mail.ru>
2022-11-28 17:08:47 +0300

    code optimization in DialogHelper

--- app/src/main/java/com/vita_zaebymba/adserviceapp/dialoghelper/DialogHelper.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/dialoghelper/DialogHelper.kt
@@ -1,6 +1,7 @@
 package com.vita_zaebymba.adserviceapp.dialoghelper
 
 import android.app.AlertDialog
+import android.view.View
 import com.vita_zaebymba.adserviceapp.MainActivity
 import com.vita_zaebymba.adserviceapp.R
 import com.vita_zaebymba.adserviceapp.accounthelper.AccountHelper
@@ -16,7 +17,31 @@ class DialogHelper(act:MainActivity) {
         val view = rootDialogElement.root // можно было сразу rootDialogElement передать в setView, но так более наглядно
         builder.setView(view)
 
+        setDialogState(index, rootDialogElement)
 
+        val dialog = builder.create()
+
+        rootDialogElement.btSignUpIn.setOnClickListener{
+            setOnClickSignUpIn(index, rootDialogElement, dialog)
+        }
+
+        dialog.show()
+    }
+
+    private fun setOnClickSignUpIn(index: Int, rootDialogElement: SignDialogBinding, dialog: AlertDialog?) {
+        dialog?.dismiss() // закрытие окна с регистрацией после нажатия на кнопку
+        if (index == DialogConst.SIGN_UP_STATE){
+
+            accHelper.signUpWithEmail(rootDialogElement.edSignEmail.text.toString(), rootDialogElement.edSignPassword.text.toString())
+
+        } else {
+            accHelper.signInpWithEmail(rootDialogElement.edSignEmail.text.toString(), rootDialogElement.edSignPassword.text.toString())
+
+        }
+
+    }
+
+    private fun setDialogState(index: Int, rootDialogElement: SignDialogBinding) {
         if (index == DialogConst.SIGN_UP_STATE){ // меняем диалоговое окно в зависимости от вохода или регистрации
             rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ad_sign_up)
             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
@@ -24,21 +49,8 @@ class DialogHelper(act:MainActivity) {
         } else {
             rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ad_sign_in)
             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_in_action)
-        }
-        val dialog = builder.create()
-
-        rootDialogElement.btSignUpIn.setOnClickListener{
-            dialog.dismiss() // закрытие окна с регистрацией после нажатия на кнопку
-            if (index == DialogConst.SIGN_UP_STATE){
-
-                accHelper.signUpWithEmail(rootDialogElement.edSignEmail.text.toString(), rootDialogElement.edSignPassword.text.toString())
-
-            } else {
-                accHelper.signInpWithEmail(rootDialogElement.edSignEmail.text.toString(), rootDialogElement.edSignPassword.text.toString())
-
-            }
+            rootDialogElement.btForgetPassword.visibility = View.VISIBLE
         }
 
-        dialog.show()
     }
 }
