96e636dbbffb74c567ac50b53ecc388f59195600
Vita <malavita29@mail.ru>
2023-02-02 14:13:54 +0300

    add viewbinding in image list fragment

--- app/src/main/java/com/vita_zaebymba/adserviceapp/fragments/ImageListFragment.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/fragments/ImageListFragment.kt
@@ -10,23 +10,28 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vita_zaebymba.adserviceapp.R
+import com.vita_zaebymba.adserviceapp.databinding.ListImageFragmentBinding
 import com.vita_zaebymba.adserviceapp.utils.ItemTouchMoveCallback
 
 class ImageListFragment(private val fragCloseInterface: FragmentCloseInterface, private val newList: ArrayList<String>): Fragment() { // этот фрагмент запускает список с картинками
+   lateinit var rootElement: ListImageFragmentBinding
+
     val adapter = SelectImageRvAdapter()
     val dragCallback = ItemTouchMoveCallback(adapter)
     val touchHelper = ItemTouchHelper(dragCallback) //класс, который будет следить за перетаскиванием элементов
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.list_image_fragment, container, false) // draw fragment
+        rootElement = ListImageFragmentBinding.inflate(inflater)
+        return rootElement.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val rcView = view.findViewById<RecyclerView>(R.id.rcViewSelectedImage)
-        touchHelper.attachToRecyclerView(rcView)
-        rcView.layoutManager = LinearLayoutManager(activity) // указываем, как элементы будут располагаться
-        rcView.adapter = adapter // присваиваем адаптер
+
+
+        touchHelper.attachToRecyclerView(rootElement.rcViewSelectedImage)
+        rootElement.rcViewSelectedImage.layoutManager = LinearLayoutManager(activity) // указываем, как элементы будут располагаться
+        rootElement.rcViewSelectedImage.adapter = adapter // присваиваем адаптер
         val updateList = ArrayList<SelectImageItem>()
         for (n in 0 until newList.size){
             updateList.add(SelectImageItem(n.toString(), newList[n]))
