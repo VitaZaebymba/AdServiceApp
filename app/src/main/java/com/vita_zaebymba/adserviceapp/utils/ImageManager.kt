6b950a8e765464e21113d4dfc9a3470d5677244c
Vita <malavita29@mail.ru>
2023-02-06 18:35:17 +0300

    checking rotation screen

--- app/src/main/java/com/vita_zaebymba/adserviceapp/utils/ImageManager.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/utils/ImageManager.kt
@@ -1,6 +1,8 @@
 package com.vita_zaebymba.adserviceapp.utils
 
 import android.graphics.BitmapFactory
+import androidx.exifinterface.media.ExifInterface
+import java.io.File
 
 object ImageManager {
 
@@ -10,6 +12,24 @@ object ImageManager {
             inJustDecodeBounds = true // берем только края картинки
         }
         BitmapFactory.decodeFile(uri, options)
-        return listOf(options.outWidth, options.outHeight)
+
+        return if (imageRotation(uri) == 90)
+            listOf(options.outHeight, options.outWidth)
+        else listOf(options.outWidth, options.outHeight)
+    }
+
+    private fun imageRotation(uri: String): Int{
+        val rotation: Int
+        val imageFile = File(uri)
+        val exif = ExifInterface(imageFile.absolutePath) // насколько был повернут экран, когда было сделано фото
+        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+        rotation= if (orientation == ExifInterface.ORIENTATION_ROTATE_90 || orientation == ExifInterface.ORIENTATION_ROTATE_270){
+            90
+        } else {
+            0
+        }
+
+        return rotation
+
     }
 }
