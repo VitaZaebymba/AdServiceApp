967150817b372653b012456fa587131629523be4
Vita <malavita29@mail.ru>
2023-02-15 21:30:11 +0300

    add data class AdClass

---
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/data/AdClass.kt
@@ -0,0 +1,12 @@
+package com.vita_zaebymba.adserviceapp.data
+
+data class AdClass( // бд поместит сразу все значения
+    val country: String? = null,
+    val city: String? = null,
+    val tel: String? = null,
+    val index: String? = null,
+    val delivery: String? = null,
+    val category: String? = null,
+    val price: String? = null,
+    val description: String? = null
+)
