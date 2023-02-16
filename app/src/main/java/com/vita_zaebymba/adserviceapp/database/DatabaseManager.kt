bd9555652d357c7e270682db20c8a37992509dba
Vita <malavita29@mail.ru>
2023-02-16 17:05:21 +0300

    roots child for ad on firebase

--- app/src/main/java/com/vita_zaebymba/adserviceapp/database/DatabaseManager.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/database/DatabaseManager.kt
@@ -1,14 +1,19 @@
 package com.vita_zaebymba.adserviceapp.database
 
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.vita_zaebymba.adserviceapp.data.Ad
 
 class DatabaseManager {
     val db = Firebase.database.getReference("main") // получение инстанции бд
+    val auth = Firebase.auth
 
     fun publishAd(ad: Ad){
-        db.setValue("Hello")
+        if (auth.uid != null) {
+            db.child(ad.key ?: "Empty").child(auth.uid!!).child("ad").setValue(ad)
+        }
+
     }
 
 }
