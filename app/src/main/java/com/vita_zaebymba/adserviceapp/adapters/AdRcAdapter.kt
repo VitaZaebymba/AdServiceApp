b6319c57f50ca9684c1c71d7920d41ea3b0897b8
Vita <malavita29@mail.ru>
2023-02-21 18:05:11 +0300

    create class AdRecyclerViewAdapter

---
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/adapters/AdRcAdapter.kt
@@ -0,0 +1,11 @@
+package com.vita_zaebymba.adserviceapp.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+
+class AdRcAdapter: RecyclerView.Adapter<AdRcAdapter.AdHolder> {
+
+
+    class AdHolder {
+
+    }
+}
