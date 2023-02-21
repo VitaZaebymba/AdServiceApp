e7ff24c3d3d733982f4848e08fbfc2f663d1197d
Vita <malavita29@mail.ru>
2023-02-21 18:41:01 +0300

    update class AdRecyclerViewAdapter

--- app/src/main/java/com/vita_zaebymba/adserviceapp/adapters/AdRcAdapter.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/adapters/AdRcAdapter.kt
@@ -1,11 +1,38 @@
 package com.vita_zaebymba.adserviceapp.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.vita_zaebymba.adserviceapp.data.Ad
+import com.vita_zaebymba.adserviceapp.databinding.AdListItemBinding
+import com.vita_zaebymba.adserviceapp.databinding.SignDialogBinding
 
-class AdRcAdapter: RecyclerView.Adapter<AdRcAdapter.AdHolder> {
+class AdRcAdapter: RecyclerView.Adapter<AdRcAdapter.AdHolder>() {
 
 
-    class AdHolder {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdHolder {
+        val binding = AdListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return AdHolder(binding) // хранит ссылки на все view
+    }
+
+    override fun onBindViewHolder(holder: AdHolder, position: Int) {
+        
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    class AdHolder(binding: AdListItemBinding): RecyclerView.ViewHolder(binding.root) { // переиспользуем элементы
+
+        fun setData(ad: Ad){
+
+        }
+
 
     }
+
 }
