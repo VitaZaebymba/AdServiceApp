8866cdcd735a1958f3e4e63ecb1dec65ac5d88ff
Vita <malavita29@mail.ru>
2023-03-14 13:06:22 +0300

    add class DiffUtilHelper

---
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/adapters/DiffUtilHelper.kt
@@ -0,0 +1,21 @@
+package com.vita_zaebymba.adserviceapp.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+
+class DiffUtilHelper: DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNewListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
