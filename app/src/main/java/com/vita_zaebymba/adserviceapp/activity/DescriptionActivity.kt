f50e4cdb4ca836ba849e75db6a836f0ebb20cbe9
Vita <malavita29@mail.ru>
2023-03-26 23:19:58 +0300

    ad page

--- app/src/main/java/com/vita_zaebymba/adserviceapp/activity/DescriptionActivity.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/activity/DescriptionActivity.kt
@@ -3,6 +3,7 @@ package com.vita_zaebymba.adserviceapp.activity
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vita_zaebymba.adserviceapp.R
 import com.vita_zaebymba.adserviceapp.adapters.ImageAdapter
 import com.vita_zaebymba.adserviceapp.databinding.ActivityDescriptionBinding
 import com.vita_zaebymba.adserviceapp.model.Ad
@@ -31,8 +32,8 @@ class DescriptionActivity : AppCompatActivity() {
     }
 
     private fun getIntentFromMainAct(){
-        val ad = intent.getSerializableExtra(AD) as Ad
-        fillImageArray(ad) // заполняем картинками
+        val ad = intent.getSerializableExtra(AD) as Ad // получаем объявление
+        updateUI(ad)
     }
 
     private fun fillImageArray(ad: Ad) { // заполнение массива ссылками из getBitmapFromUri (class ImageManager)
@@ -41,6 +42,30 @@ class DescriptionActivity : AppCompatActivity() {
             val bitmapList = ImageManager.getBitmapFromUris(listUris)
             adapter.update(bitmapList as ArrayList<Bitmap>) // обновляем адаптер
         }
+    }
+
+    private fun updateUI(ad: Ad) {
+        fillImageArray(ad) // заполняем картинками
+        fillTextViews(ad)
+    }
+
+    private fun fillTextViews(ad:Ad) = with(binding) {
+        tvTitleDescription.text = ad.title
+        tvPriceD.text = ad.price
+        tvTelD.text = ad.tel
+        tvCountryD.text = ad.country
+        tvCityD.text = ad.city
+        tvIndexD.text = ad.index
+        tvDeliveryD.text = isDelivery(ad.delivery.toBoolean())
+        tvDescription.text = ad.description
+
+    }
+
+    private fun isDelivery(withDelivery: Boolean): String {
+        val yes = getString(R.string.yes)
+        val no = getString(R.string.no)
+        return if (withDelivery) yes else no
+
     }
 
     companion object{
