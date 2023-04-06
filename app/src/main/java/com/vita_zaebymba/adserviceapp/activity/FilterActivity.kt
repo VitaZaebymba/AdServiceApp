8afd7de0cce9981f37b5ece073d50f7396f229c8
Vita <malavita29@mail.ru>
2023-04-06 13:36:34 +0300

    check2 filter activity

--- app/src/main/java/com/vita_zaebymba/adserviceapp/activity/FilterActivity.kt
+++ app/src/main/java/com/vita_zaebymba/adserviceapp/activity/FilterActivity.kt
@@ -2,6 +2,7 @@ package com.vita_zaebymba.adserviceapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.vita_zaebymba.adserviceapp.R
 import com.vita_zaebymba.adserviceapp.databinding.ActivityFilterBinding
 
@@ -12,5 +13,16 @@ class FilterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityFilterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        actionBarSettings()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun actionBarSettings(){
+        val ab = supportActionBar
+        ab?.setDisplayHomeAsUpEnabled(true)
     }
 }
